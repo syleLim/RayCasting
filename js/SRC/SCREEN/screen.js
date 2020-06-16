688af1a6b14951e6f7ae998c1f72241663937d03
syleLim <syle6492@gmail.com>
2020-06-16 17:37:52 +0900

    image change

--- js/SRC/SCREEN/screen.js
+++ js/SRC/SCREEN/screen.js
@@ -13,9 +13,9 @@ class Screen {
 			index	: index,
 			start	: this.height / 2 - ((1 / length) / 2) * this.width,
 			end		: this.height / 2 + ((1 / length) / 2) * this.width,
-			r	: isHit === X ? 0xff : 0x77,
-			g	: isHit === X ? 0xff : 0x77,
-			b	: isHit === X ? 0xff : 0x77,
+			r	: isHit === X ? 0x77 : 0x33,
+			g	: isHit === X ? 0x77 : 0x33,
+			b	: isHit === X ? 0x77 : 0x33,
 			a	: 255,
 		})
 	}
